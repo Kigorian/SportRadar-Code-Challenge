bbffec0cc1790b987a72214c96106f49b29c2cbc
Kigorian <36802041+Kigorian@users.noreply.github.com>
2021-04-06 17:22:10 -0500

    Got a working POC. Added some packages and assemblies to get the console to hit the API, and print some results to the console.

--- NHL-API/Program.cs
+++ NHL-API/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Configuration;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
 
 namespace NHL_API
 {
@@ -6,7 +12,37 @@ namespace NHL_API
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string url = "https://statsapi.web.nhl.com/api/v1/teams";
+            //string url = $"{ConfigurationManager.AppSettings["NHL-API"]}/teams";
+            string json = GetJsonResponse(url);
+
+            var jObject = JObject.Parse(json);
+            var teams = jObject["teams"];
+
+            Console.WriteLine("ID: " + teams[0]["id"]);
+            Console.WriteLine("Name: " + teams[0]["name"]);
+            Console.WriteLine("Link: " + teams[0]["link"]);
+            Console.WriteLine("Venue Name: " + teams[0]["venue"]["name"]);
+        }
+
+        /// <summary>
+        /// Calls the API for the given url, and returns a string containing the JSON response.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static string GetJsonResponse(string url)
+        {
+            HttpWebRequest webrequest = (HttpWebRequest)WebRequest.Create(url);
+            webrequest.Method = "GET";
+            webrequest.ContentType = "application/x-www-form-urlencoded";
+
+            HttpWebResponse webresponse = (HttpWebResponse)webrequest.GetResponse();
+            Encoding enc = Encoding.GetEncoding("utf-8");
+            StreamReader responseStream = new StreamReader(webresponse.GetResponseStream(), enc);
+            string result = responseStream.ReadToEnd();
+            webresponse.Close();
+
+            return result;
         }
     }
 }
